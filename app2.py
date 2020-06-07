5c4216039440fc8e274c4a7b12bb61d5cb7f2dcd
asaymp <amothapollock@marsdd.com>
2020-06-07 18:20:04 -0400

    Added map and bar chart

--- app2.py
+++ app2.py
@@ -1,50 +1,48 @@
 import pandas as pd
-import requests
-import io
 import streamlit as st
+import numpy as np
+import plotly.express as px
 
-from geopy.geocoders import Nominatim
+data_url = "https://raw.githubusercontent.com/asaymp/TO_green_tech/master/full_data.csv"
+
+st.title("Sloar water heaters and green roofs in Toronto")
+st.markdown("This application is a Streamlit dashboard that can be used to locate commercial and residential solar heaters and green roofs in Toronto. Data is from the City of Toronto's open data portal.")
+
+@st.cache(persist=True)
+def load_data():
+    data = pd.read_csv(data_url)
+    data.dropna(subset=['latitude', 'longitude'], inplace=True)
+    return data
+
+data = load_data()
+
+#map of completed projects - solar heaters or green roofs
+closed_projects = data[data['status']=='Closed']
+st.header("Where are the green building technologies in Toronto?")
+techtype = st.selectbox("Technology Type", ('Solar Water Heater', 'Green Roof'))
+st.map(closed_projects.query("green_tech == @techtype")[["latitude","longitude"]].dropna(how="any"))#have to be latitude and longitude columns if we want to make a map
+
+#barchart top neighbourhoods by number of solar heaters/green roofs
+st.subheader("Top five neighbourhoods by number of installations") #
+data['neighbourhood'] = data['location'].apply(lambda x: x.split(",")[4].strip())
+
+if techtype == 'Solar Water Heater':
+    nb_series = data['neighbourhood'][data['green_tech']=='Solar Water Heater']
+else:
+    nb_series = data['neighbourhood'][data['green_tech']=='Green Roof']
+
+chart_data = nb_series.value_counts().rename_axis('neighbourhood').reset_index(name='count').sort_values(by='count', ascending=False).head(5)
+
+fig = px.bar(chart_data, x='neighbourhood', y='count', hover_data=['count'], height=400)
+st.write(fig)
 
 
-class GreenToronto():
+#linechart by completed projects over time solar heaters/green roofs
 
-    def __init__(self):
 
-        self.heaters_url = ('https://raw.githubusercontent.com/asaymp/TO_green_tech/master/Solar%20Hot%20Water%20Heater%20Data.csv')
-        self.rooves_url = ('https://raw.githubusercontent.com/asaymp/TO_green_tech/master/Building%20Permits%20-%20Green%20Roofs%20Data.csv')
 
-        st.title("Sloar water heaters and green rooves in Toronto")
-        st.markdown("This application is a Streamlit dashboard that can be used to locate commercial and residential solar heaters and green rooves in Toronto. Data is from the City of Toronto's open data portal.")
 
-    @st.cache(persist=True)
-    def load_data(self):
-        heater_download = requests.Session().get(self.heaters_url).content
-        heater_data = pd.read_csv(io.StringIO(heater_download.decode('utf-8')))
-        heater_data['green_tech'] = 'Solar Water Heater'
-        roof_download = requests.Session().get(self.rooves_url).content
-        heater_data = pd.read_csv(io.StringIO(roof_download.decode('utf-8')))
-        roof_data['green_tech'] = 'Green Roof'
-        data = pd.concat([heater_data, roof_data], ignore_index=True)
-        lowercase = lambda x: str(x).lower()
-        data.rename(lowercase, axis='columns', inplace=True)
-        data['heater_work'] = data['work']
-        data.drop('work', axis=1, inplace=True)
-        date_cols = ['application_date','completed_date','issued_date']
-        for col in date_cols:
-            data[col] = pd.to_datetime(data[col])
-            data[col] = [date.date() for date in data[col]]
-        data['address'] = data['street_num'] + ' ' + data['street_name'] + ' ' + data['street_type'] + data['street_direction'] + ' Toronto, Canada'
-        #geolocator = Nominatim(user_agent='')
-        #data['latitude'] = geolocator.geocode(data['address']).latitude
-        #data['longitude'] = geolocator.geocode(data['address']).longitude
-        #pd.to_csv('green_Toronto_full_data.csv')
-        return data
-
-if __name__ == '__main__':
-    gt = GreenToronto()
-    gt.load_data()
 #figure out how to pull the data through the City of Toronto's APIs
-
 #attribution to Open Street Map's geolocator feature
 #attribution to Deck
 #attribution to Streamlit
